4e1a77b8499586ab4e01136e3d9a8a76736950a5
jjvernaza <125580288+jjvernaza@users.noreply.github.com>
2024-07-26 10:17:16 -0500

    Update 26 julio db_config.py

--- db_config.py
+++ db_config.py
@@ -1,4 +1,11 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+# Configuración del motor
 def get_engine():
-    return create_engine('mysql+pymysql://root:WFClJvsXnNlTyZGbqXFIofJymrvmZpNk@viaduct.proxy.rlwy.net:10602/railway')
+    return create_engine('mysql+pymysql://avnadmin:AVNS_5uSGQcVeHQEIRQ1YA8b@mysql-f2a31f6-jjvernazamayor-66da.g.aivencloud.com:19544/vozip_database')
+
+# Crear una sesión
+engine = get_engine()
+Session = sessionmaker(bind=engine)
+session = Session()
