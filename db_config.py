543c251e48e31626f330bf4636135ad6d9773401
jjvernaza <125580288+jjvernaza@users.noreply.github.com>
2024-09-18 17:45:37 -0500

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,11 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 # Configuración del motor
 def get_engine():
     return create_engine('mysql+pymysql://u8cwb0paehh1l8ue:ALFHA1EO6IWfl7Hm7Unm@buqpques8tk1uvbfib4j-mysql.services.clever-cloud.com:3306/buqpques8tk1uvbfib4j')
 
-# Crear una sesión
-engine = get_engine()
-Session = sessionmaker(bind=engine)
-session = Session()
